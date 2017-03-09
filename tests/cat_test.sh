62f118011ac4e3e7cf5fc57a5d3f003046728967
James Brosnahan <jb172@zips.uakron.edu>
2017-03-09 21:13:18 +0000

    added test scripts

--- tests/cat_test.sh
+++ tests/cat_test.sh
@@ -1,3 +1,15 @@
-#!/bin/env bash
+#!/bin/bash
+pwd
 
+$OUTPUT=../new/cat.txt
+$ORIGINAL=../old/cat.txt
+$SHELL_SCRIPT=$(../scripts/cat.sh)
 
+$SHELL_SCRIPT >> $OUTPUT
+
+$DIFF=$(diff $OUTPUT $ORIGINAL)
+
+if [ $DIFF == ""]
+then
+	echo "It worked"
+fi
